ef39b8f02387197b3bcc0273169bcf099cdaadb9
viraj tamakuwala <soulr27@gmail.com>
2024-02-19 23:20:53 +0100

    added variables file

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "argo-cd-backend-tetris" # Replace with your actual S3 bucket name
+    bucket = var.bucket # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "us-east-1"
+    region = var.aws_region
   }
 }
