52ca3ffc5f7050b7b89f7f33ee4e8d6501558998
viraj tamakuwala <soulr27@gmail.com>
2024-02-20 14:33:10 +0100

    fix: aws region

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "argo-cd-backend-tetris" # Replace with your actual S3 bucket name
+    bucket = "argo-cd-backend-tetris-1" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "argo-cd-backend-tetris" # Replace with your actual S3 bucket name
+    bucket = "argo-cd-backend-tetris-1" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
