f69a2a4ade66775531c1ea481a64e3f7de4ac924
viraj tamakuwala <soulr27@gmail.com>
2024-04-12 21:49:10 +0200

    update

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "argo-cd-backend-tetris-1" # Replace with your actual S3 bucket name
+    bucket = "argo-cd-backend-tetris"
     key    = "eks/terraform.tfstate"
     region = "us-east-2"
   }
